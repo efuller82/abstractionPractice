e4d57705c7e98bd5bec69ae2ddca28d1f0ef70e3
Eric Fuller <efuller82@gmail.com>
2019-12-26 21:27:28 -0600

    Add project files.

---
+++ abstractionPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyAbstractClass abClass = new MyAbstractClass(); 
+            classA a = new classA();
+            a.PrintMessage();
+            a.PrintMessage();
+            Console.Read();
+        }
+    }
+    abstract class MyAbstractClass
+    {
+        private string message = "Hello C#";
+        public void PrintMessage()
+        {
+            Console.WriteLine(message);
+        }
+        public abstract void PrintMessageAbstract();
+    }
+    class ClassA : MyAbstractClass
+    {
+        public override void PrintMessageAbstract()
+            {
+                Console.WriteLine("C# is fun!");
+            }
+    }
+}
